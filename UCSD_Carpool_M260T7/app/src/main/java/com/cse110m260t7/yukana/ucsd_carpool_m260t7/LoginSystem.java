8c2c0af809d7c19cb16a79fad155a0bfa4c97516
panzhengren <pzr2864785@sohu.com>
2016-02-13 13:05:18 -0800

    updated

---
+++ UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
@@ -0,0 +1,10 @@
+package com.cse110m260t7.yukana.ucsd_carpool_m260t7;
+
+/**
+ * Created by panzhengren on 16/2/13.
+ */
+public class LoginSystem {
+    public void loginWithEmail(String email, String password){
+
+    }
+}
