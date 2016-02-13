b3421e7efbd46d036591f10628e06e479f11fb40
panzhengren <pzr2864785@sohu.com>
2016-02-13 14:36:24 -0800

    added firebase sdk

--- UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
+++ UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
@@ -3,6 +3,7 @@ package com.cse110m260t7.yukana.ucsd_carpool_m260t7;
 /**
  * Created by panzhengren on 16/2/13.
  */
+
 public class LoginSystem {
     public void loginWithEmail(String email, String password){
 
