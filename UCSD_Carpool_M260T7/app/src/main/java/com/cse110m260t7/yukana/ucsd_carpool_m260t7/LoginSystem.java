ba6d577f5fa97d4f83b361a238359c1362924b96
panzhengren <pzr2864785@sohu.com>
2016-02-13 15:34:28 -0800

    Login System

--- UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
+++ UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
@@ -4,25 +4,35 @@ package com.cse110m260t7.yukana.ucsd_carpool_m260t7;
  * Created by panzhengren on 16/2/13.
  */
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import java.util.*;
 
 public class LoginSystem {
 
-    public void loginWithEmail(String email, String password){
-        Firebase ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
-        ref.createUser("bobtony@firebase.com", "correcthorsebatterystaple", new Firebase.ValueResultHandler<Map<String, Object>>() {
-            @Override
+    public void createUser(String email, String password){
+        Firebase ref = new Firebase("https://ucsdcarpool.firebaseio.com");
+        ref.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
             public void onSuccess(Map<String, Object> result) {
                 System.out.println("Successfully created user account with uid: " + result.get("uid"));
             }
-            @Override
             public void onError(FirebaseError firebaseError) {
-                // there was an error
+                System.out.println("Error creating user!");
             }
         });
     }
 
-    public void createUser(String email, String password){
-
+    public void loginWithEmail(String email, String password){
+        Firebase ref = new Firebase("https://ucsdcarpool.firebaseio.com");
+        ref.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            public void onAuthenticated(AuthData authData) {
+                System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+            }
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                System.out.println("Error logining in!");
+            }
+        });
     }
+    
 }
