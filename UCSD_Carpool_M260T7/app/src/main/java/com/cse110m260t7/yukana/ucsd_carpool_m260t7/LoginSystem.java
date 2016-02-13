9cd1f754bea6b70927ff310527199c74b43bb96b
panzhengren <pzr2864785@sohu.com>
2016-02-13 15:13:08 -0800

    updated

--- UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
+++ UCSD_Carpool_M260T7/app/src/main/java/com/cse110m260t7/yukana/ucsd_carpool_m260t7/LoginSystem.java
@@ -4,8 +4,25 @@ package com.cse110m260t7.yukana.ucsd_carpool_m260t7;
  * Created by panzhengren on 16/2/13.
  */
 
+import com.firebase.client.Firebase;
+
 public class LoginSystem {
+
     public void loginWithEmail(String email, String password){
+        Firebase ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+        ref.createUser("bobtony@firebase.com", "correcthorsebatterystaple", new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+                System.out.println("Successfully created user account with uid: " + result.get("uid"));
+            }
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                // there was an error
+            }
+        });
+    }
+
+    public void createUser(String email, String password){
 
     }
 }
