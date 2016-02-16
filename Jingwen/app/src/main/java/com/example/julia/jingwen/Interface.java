53ad982ae4546c8a7e882f4d52a33625ec0ad892
Jingwen Xie <jix111@eng.ucsd.edu>
2016-02-15 19:44:12 -0800

    modified UI

--- Jingwen/app/src/main/java/com/example/julia/jingwen/Interface.java
+++ Jingwen/app/src/main/java/com/example/julia/jingwen/Interface.java
@@ -1,8 +1,11 @@
 package com.example.julia.jingwen;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,67 +23,72 @@ public class Interface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        //show/hide fragments onclick button
-        FragmentManager fm = getFragmentManager();
-        addShowHideListener(R.id.pafraghide, fm.findFragmentById(R.id.pafrag));
-        addShowHideListener (R.id.drifraghide, fm.findFragmentById(R.id.drifrag));
-
-    }
-    /**Called when user click passenger button*/
-    void addShowHideListener (final int buttonId, final Fragment fragment){
-        final Button button = (Button)findViewById(buttonId);
-        button.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.setCustomAnimations(android.R.animator.fade_in,
-                        android.R.animator.fade_out);
-                if (fragment.isHidden()) {
-                    ft.show(fragment);
-                    button.setText("Hide");
-                } else {
-                    ft.hide(fragment);
-                    if (buttonId == R.id.pafraghide)
-                        button.setText("show your active carpools");
-                    else
-                        button.setText("show your active customers");
-                }
-                ft.commit();
-            }
-        });
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        ViewPager mPager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
 
+    private class MyPagerAdapter extends FragmentPagerAdapter {
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_interface, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MyPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public android.support.v4.app.Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return FirstFragment.newInstance("FirstFragment, Instance 1");
+                case 1: return SecondFragment.newInstance("SecondFragment, Instance 1");
+                case 2: return ThirdFragment.newInstance("ThirdFragment, Instance 1");
+                default: return ThirdFragment.newInstance("ThirdFragment, Default");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
 }
