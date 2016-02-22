18f6dfa106394452db420010b2aea7332a51a5fc
juliajwx <jix111@eng.ucsd.edu>
2016-02-22 12:38:05 -0800

    fix UI

---
+++ Jingwen/app/src/main/java/com/example/julia/jingwen/SecondFragment.java
@@ -0,0 +1,35 @@
+package com.example.julia.jingwen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by xiejingwen on 2/22/16.
+ */
+public class SecondFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.first_frag, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragSecond);
+        tv.setText(getArguments().getString("msg"));
+
+        return v;
+    }
+
+    public static FirstFragment newInstance(String text) {
+
+        FirstFragment f = new FirstFragment();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+}
