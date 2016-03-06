25a1150189c45afe6fb144b2c800e8329224d3d7
yuz100086 <yuz287@ucsd.edu>
2016-03-06 14:50:16 -0800

    need to pull

---
+++ UCSDCarpool 3.0/app/src/androidTest/java/com/example/jem/ucsdcarpool/loginTest.java
@@ -0,0 +1,26 @@
+package com.example.jem.ucsdcarpool;
+
+/**
+ * Created by Yukana on 16/3/6.
+ */
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+//@Rule
+//public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+@RunWith(AndroidJUnit4.class)
+
+public class loginTest{
+    @ClassRule
+    public static DisableAnimationsRule disableAnimation;
+
+    public void loginAcitityTest() {
+    // Do your testing
+
+
+    }
+}
