427367ea7f56c58adb4b7be8361e236b851eb4bf
Jie Fu <jif0729@gmail.com>
2016-03-06 23:06:29 -0800

    add test

--- UCSDCarpool 3.0/app/src/androidTest/java/com/example/jem/ucsdcarpool/RegisterTest.java
+++ UCSDCarpool 3.0/app/src/androidTest/java/com/example/jem/ucsdcarpool/RegisterTest.java
@@ -1,12 +1,12 @@
 package com.example.jem.ucsdcarpool;
 
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -16,7 +16,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 /**
  * Created by Jem on 3/6/16.
  */
@@ -26,8 +26,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class RegisterTest {
     @ClassRule
     public static DisableAnimationsRule disableAnimation;
-    ActivityTestRule registerrule = new ActivityTestRule(Register.class);
 
+    @Rule
+    public ActivityTestRule<Register> registerrule = new ActivityTestRule<>(Register.class);
+
+
+    @Test
     public void RegisterAcitityTest() {
         onView(withId(R.id.user_name))
                 .perform(typeText("fjfjfj"), closeSoftKeyboard());
@@ -35,10 +39,21 @@ public class RegisterTest {
                 .perform(typeText("fj"), closeSoftKeyboard());
         onView(withId(R.id.user_email))
                 .perform(typeText("fj@fj.com"), closeSoftKeyboard());
-        onView(withId(R.id.register))
-                .perform(click()).check(matches(isDisplayed()));
+        //onView(withId(R.id.register))
+        //        .perform(click()).check(matches(isDisplayed()));
+        //onView(withId(R.id.reset))
+        //        .perform(click()).check(matches(isDisplayed()));
+        onView(withId(R.id.user_name)).check(matches(withText("fjfjfj")));
+        onView(withId(R.id.user_password)).check(matches(withText("fj")));
+        onView(withId(R.id.user_email)).check(matches(withText("fj@fj.com")));
+
         onView(withId(R.id.reset))
                 .perform(click()).check(matches(isDisplayed()));
+
+        onView(withId(R.id.user_name)).check(matches(withText("")));
+        onView(withId(R.id.user_password)).check(matches(withText("")));
+        onView(withId(R.id.user_email)).check(matches(withText("")));
+
     }
 }
 
