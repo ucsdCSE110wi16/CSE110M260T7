44503bf7d1b5f5dd6c65d21ad2706077a8d1ef56
Jie Fu <jif0729@gmail.com>
2016-03-06 21:04:28 -0800

    logout add

--- UCSDCarpool 3.0/app/src/main/java/com/example/jem/ucsdcarpool/Menu.java
+++ UCSDCarpool 3.0/app/src/main/java/com/example/jem/ucsdcarpool/Menu.java
@@ -6,10 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by Jem on 3/2/16.
  */
 public class Menu extends AppCompatActivity {
+    private Firebase mRef = new Firebase("");
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,7 @@ public class Menu extends AppCompatActivity {
         Button mybutton3 = (Button) findViewById(R.id.home_Find_Schedule);
         Button mybutton4 = (Button) findViewById(R.id.home_main);
         Button mybutton5 = (Button) findViewById(R.id.home_Find_Schedule_Driver);
+        Button mybutton6 = (Button) findViewById(R.id.home_Log_Out);
 
         mybutton1.setOnClickListener(new View.OnClickListener() {
 
@@ -54,6 +58,16 @@ public class Menu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Menu.this, Find_schedule_driver.class);
+                startActivity(intent);
+            }
+        });
+
+        mybutton6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mRef.unauth();
+                Intent intent = new Intent(Menu.this, Log_in.class);
                 startActivity(intent);
             }
         });
